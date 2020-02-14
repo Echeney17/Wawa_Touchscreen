64a81bf7ec26974a004e7521f053031fc402883a
AsraaAlkurdi <axa70@psu.edu>
2020-02-14 18:13:27 -0500

    Added Beverges Window

---
+++ Wawa_Touchscreen/Beverages.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wawa_Touchscreen
+{
+    /// <summary>
+    /// Interaction logic for Beverages.xaml
+    /// </summary>
+    public partial class Beverages : Window
+    {
+        public Beverages()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFruitSmoothies_Click(object sender, RoutedEventArgs e)
+        {
+            Beverages firstBeverage = new Beverages();
+            firstBeverage.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChaiTeas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
